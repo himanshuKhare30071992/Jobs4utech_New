5999620d9f29fcb936dc690aae318d307c14b5b3
Himanshu <himanshukhare1992@gmail.com>
2020-11-11 19:44:32 +0530

    #3 Date added in output file

---
+++ src/main/java/operations/DatePrint.java
@@ -0,0 +1,26 @@
+package operations;
+
+import java.util.Date;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DatePrint 
+{
+	
+	public static void main(String[] args) 
+	{
+		WebDriver wd = new ChromeDriver();
+		wd.close();
+		
+		
+	
+		wd = new FirefoxDriver();
+		wd.close();
+		
+		
+		
+	}
+
+}
--- src/main/java/puneJobs/PuneTestingJobs.java
+++ src/main/java/puneJobs/PuneTestingJobs.java
@@ -1,6 +1,8 @@
 package puneJobs;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -14,12 +16,11 @@ import org.openqa.selenium.support.ui.Select;
 
 public class PuneTestingJobs 
 {
-	
-	
-	
 	public static void main(String[] args) throws Exception
 	{
 		String completeData ="";
+		boolean siteStatus = false;
+		
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://jobs4utech.in/");
@@ -27,55 +28,79 @@ public class PuneTestingJobs
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("//a[@href='#citiesdrpdwn']")).click();
 		
-		driver.findElement(By.xpath("//a[text()='Pune']")).click();
-		
-		driver.findElement(By.xpath("//a[@class='btn btn-info btn-sm m-1']//i")).click();
-		
-		WebElement domainFilter  = driver.findElement(By.id("category"));
-		domainFilter.click();
-		Select sc = new Select(domainFilter);
-		sc.selectByValue("software testing");
-		
-		driver.findElement(By.xpath("//button[@id='applyfilter']")).click();
-		
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		for(int i=1; i<=100; i++)
+		try 
 		{
-			//System.out.println(i+">"+js.executeScript("scroll(0,"+i*1000+")"));
-			js.executeScript("scroll(0,"+i*1000+")");
-			Thread.sleep(10);
-		}
+			WebElement ele = driver.findElement(By.xpath("//span[text()='No internet']"));
+			System.out.println("Internet issue. . . . . -:( Status ---> "+ele.isDisplayed());
+			driver.quit();
+					
+		} 
+		catch (Exception e) 
+		{
+				//WebElement ele =	driver.findElement(By.xpath("//span[text()='This site can’t be reached']"));
 
-		WebElement jobFound = driver.findElement(By.xpath("//h5[text()='Found ']//span"));
-		//String puneJobsCount = jobFound.getText();
-		
-		System.out.println("--------------------------------------------------------------");
-		
-		List<WebElement> listOfPune =	driver.findElements(By.xpath("//a[text()='Pune, India']//following::span[@class='pl-1'][1]"));
-		int listSize = listOfPune.size();
-		System.out.println("Total Software Testing Jobs available for Pune city = "+listSize);	
-		
-		File destinationFile = new File("../Jobs4U_New/OutputFile/PuneJobsData.txt");
-		
-		for(int  i=0; i<listSize; i++)
-		{
-			WebElement ele =	listOfPune.get(i);
-			String eleText = ele.getText();
-			System.out.println("#"+i+"--->>> "+eleText);
-			System.out.println("--------------------------------------------------------------");
-			completeData = completeData+"\n"+ eleText;
-		}
-		
-		System.out.println("Temp = "+completeData);
-		
-		FileUtils.writeStringToFile(destinationFile, "Total Pune Jobs = "+listSize+"\n"+completeData);
-		
-		ProcessBuilder	pb = new ProcessBuilder("notepad.exe", "..\\Jobs4U_New\\\\OutputFile\\\\PuneJobsData.txt");
-		pb.start();	
-		
-		driver.quit();
+				System.out.println("Running my code. . . .");
+				
+				Thread.sleep(2000);
+				driver.findElement(By.xpath("//a[@href='#citiesdrpdwn']")).click();
+				
+				Thread.sleep(2000);
+				driver.findElement(By.xpath("//a[text()='Pune']")).click();
+				
+				driver.findElement(By.xpath("//a[@class='btn btn-info btn-sm m-1']//i")).click();
+				
+				WebElement domainFilter  = driver.findElement(By.id("category"));
+				domainFilter.click();
+				Select sc = new Select(domainFilter);
+				sc.selectByValue("software testing");
+				
+				driver.findElement(By.xpath("//button[@id='applyfilter']")).click();
+				
+				JavascriptExecutor js = (JavascriptExecutor) driver;
+				for(int i=1; i<=100; i++)
+				{
+					//System.out.println(i+">"+js.executeScript("scroll(0,"+i*1000+")"));
+					js.executeScript("scroll(0,"+i*1000+")");
+					Thread.sleep(10);
+				}
+
+				WebElement jobFound = driver.findElement(By.xpath("//h5[text()='Found ']//span"));
+				//String puneJobsCount = jobFound.getText();
+				
+				System.out.println("--------------------------------------------------------------");
+				
+				List<WebElement> listOfPune =	driver.findElements(By.xpath("//a[text()='Pune, India']//following::span[@class='pl-1'][1]"));
+				int listSize = listOfPune.size();
+				System.out.println("Total Software Testing Jobs available for Pune city = "+listSize);	
+				
+				File destinationFile = new File("../Jobs4U_New/OutputFile/PuneJobsData.txt");
+				
+				for(int  i=0; i<listSize; i++)
+				{
+					WebElement ele =	listOfPune.get(i);
+					String eleText = ele.getText();
+					System.out.println("#"+i+"--->>> "+eleText);
+					System.out.println("--------------------------------------------------------------");
+					completeData = completeData+"\n"+ eleText;
+				}
+				
+				System.out.println("Temp = "+completeData);
+				
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMMM-YYYY hh:mm:ss a");
+				Date date = new Date();
+				String currentDate = dateFormat.format(date);
+				System.out.println(currentDate);
+				
+				
+				FileUtils.writeStringToFile(destinationFile, "#Date: "+currentDate+ "\n\n#Total Testing Jobs in Pune = "+listSize+"\n"+completeData);
+				
+				ProcessBuilder	pb = new ProcessBuilder("notepad.exe", "..\\Jobs4U_New\\OutputFile\\PuneJobsData.txt");
+				pb.start();	
+
+			}
+			
+		//driver.quit();
 
 	}
 
